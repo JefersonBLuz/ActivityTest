c99c276730d8fca2bb8e0ba1e415020ce3f4bb58
rafael santana <rafael.s.santos6@ba.estudante.senai.br>
2024-11-05 21:06:56 -0300

    Modificado arquivo cypress.config.js

--- cypress/e2e/ActivityTest.cy.js
+++ cypress/e2e/ActivityTest.cy.js
@@ -1,20 +1,15 @@
+
+
 describe('ActivityTest', () => {
-beforeEach(() => {
-  cy.visit('http://localhost:3000')
-})
+  beforeEach (() => {
+    cy.visit('http://localhost:8082') 
+  })
+ it('Teste de cadastro', () => {
+  cy.await(2000)
+  
 
-
- it('Cenários de teste com Cypress', () => {
-  cy.get('#email').type("rafael@email.com")
-  cy.get('[type="password"]').type('Rafael123')
-  cy.get('[type="button"] > a').click()
-  cy.get('#firstName').type("Rafael")
-  cy.get('#lastName').type("Santana")
-  cy.get('#email').type('rafael@email.com')
-  cy.get('#password').type('Rafael123')
-  cy.get('#confirmPassword').type('Rafael123')
-  cy.get('#cpf').type('000.000.000-00')
-  cy.get('button').click()
+  
+    
 
 
   })
